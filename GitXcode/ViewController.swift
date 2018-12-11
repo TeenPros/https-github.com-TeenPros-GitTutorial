5968132305348a6082e3bbdb66c58f1eecb03a26
Dan Hornung <danbrown@dans-macbook-pro.local>
2018-12-11 17:07:13 -0500

    Initial Commit

---
+++ GitXcode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitXcode
+//
+//  Created by Dan Brown on 12/11/18.
+//  Copyright © 2018 Dan Brown. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
